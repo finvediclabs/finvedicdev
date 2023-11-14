f9057a6cef48942b50d3fc50475ce2f7b4cec0b2
finvedicdev <sony.sandeep7890@gmail.com>
2023-11-14 23:57:45 +0530

    upload data is added

--- src/components/createFiles.vue
+++ src/components/createFiles.vue
@@ -4,8 +4,36 @@
       <fin-portlet-heading :loading="loading" backArrow> {{ queryData.title }}</fin-portlet-heading>
     </fin-portlet-header>
     <fin-portlet-item>
-      <q-form @submit="onSubmit">
+      <q-form @submit="validatePostData">
         <div class="row justify-center">
+          <div class="col-10 q-pb-lg">
+            <div class="row">
+              <div v-for="category in categories" class="col q-pa-sm">
+                <q-btn :label="category.categoryName" no-caps v-if="!subCategories[category.id]" class="full-width"
+                  size="lg" :class="selectedCategory?.id == category.id ? 'bg-finvedic text-white' : ''"
+                  @click="selectCategory(category)" />
+
+                <q-btn-dropdown :label="category.categoryName" no-caps v-if="subCategories[category.id]"
+                  class="full-width" :class="{ 'bg-finvedic text-white': selectedCategory?.id === category.id }"
+                  size="lg">
+                  <q-list>
+                    <q-item v-for="subCategory in subCategories[category.id]" clickable v-close-popup
+                      @click="selectSubCategory(category, subCategory)"
+                      :class="{ 'bg-finvedic text-white': selectedSubCategory?.id == subCategory.id }">
+                      <q-item-section>
+                        <q-item-label><b>{{ subCategory.subCategoryName }}</b></q-item-label>
+                      </q-item-section>
+                    </q-item>
+                  </q-list>
+                </q-btn-dropdown>
+                <div v-if="selectedCategory?.id && subCategories[selectedCategory.id] && !selectedSubCategory?.id">
+                  {{ error[selectedCategory.id] }}
+                </div>
+              </div>
+            </div>
+            <div v-if="!selectedCategory?.id" class="text-red" style="height: 20px;font-size: 14px;">{{ error.category }}
+            </div>
+          </div>
           <div class="col-12 col-md-6 ">
             <fin-portlet-item class="full-width q-pa-md">
               <q-input v-model="title" filled label="Title" lazy-rules
@@ -41,14 +69,15 @@ import FinPortletItem from "src/components/Portlets/FinPortletItem.vue";
 import DropFile from "src/components/dropFile/DropFile.vue"
 import { useProfileStore } from "src/stores/profile";
 import { storeToRefs } from "pinia";
+import { useCategorieStore } from "src/stores/Categories";
 import CryptoJS from 'crypto-js'
 export default {
   setup() {
     const profileStore = useProfileStore();
     const { profile } = storeToRefs(profileStore);
-    return {
-      profile
-    }
+    const categorieStore = useCategorieStore();
+    const { categories, subCategories } = storeToRefs(categorieStore);
+    return { profile, categories, subCategories }
   },
   components: {
     FinPortlet,
@@ -65,6 +94,10 @@ export default {
       errorFile: '',
       loading: false,
       queryData: {},
+      selectedCategory: {},
+      selectedSubCategory: {},
+      videoCoverPath: '',
+      error: {}
     }
   },
   mounted() {
@@ -92,19 +125,53 @@ export default {
     ChangeCover() {
       this.cover = this.$refs.file.values;
     },
-    onSubmit() {
-      if (!this.cover.length) {
+    validatePostData() {
+      console.log(this.cover,this.selectedCategory?.id );
+      if (this.cover.length && this.selectedCategory?.id) {
+        alert('aaaaaaaaaaa')
+        if (this.subCategories[this.selectedCategory?.id]) {
+          alert('sub Cat');
+          if (this.selectedSubCategory?.id) {
+            this.uploadImg();
+          } else {
+
+            this.error[this.selectedCategory?.id] = 'Choose An Option'
+          }
+        } else {
+          alert('category');
+          this.uploadImg();
+        }
+      } else {
+        alert('saaaaadddddddddddddddddddddddd')
         this.errorFile = "Image Is required";
-        return false;
+        this.error.category = "Please Select An Option";
       }
+    },
+    uploadImg() {
+      alert('hai');
       let formData = new FormData();
-      formData.append('accountId', this.profile?.id);
-      formData.append('description', this.description);
-      formData.append('heading', this.title);
-      // formData.append('image', this.cover[0]);
+      formData.append('file', this.cover[0]);
+      this.$api.post('fs/upload-file', formData).then(response => {
+        this.videoCoverPath = response.data.uri;
+        this.onSubmit();
+      }).catch(error => {
+        this.showMsg(error.response?.data.message || error.message, 'negative');
+      })
+    },
+    onSubmit() {
+      alert('called');
+      let request = {
+        accountId: this.profile?.id,
+        description: this.description,
+        heading: this.title,
+        videoCoverPath: this.videoCoverPath,
+        categoryId: this.selectedCategory.id
+      }
+      request.subCategory = this.selectedSubCategory.id;
+
       if (!this.loading) {
         this.loading = true;
-        this.$api.post(queryData.url, formData).then(response => {
+        this.$api.post(this.queryData.url, request).then(response => {
           this.loading = false;
           if (response.data.success) {
             this.showMsg(response.data?.message, 'positive');
@@ -121,6 +188,14 @@ export default {
     selectFilesData(val) {
       this.cover = val;
     },
+    selectCategory(category) {
+      this.selectedCategory = category;
+      this.selectedSubCategory = {};
+    },
+    selectSubCategory(category, subCategory) {
+      this.selectedCategory = category;
+      this.selectedSubCategory = subCategory;
+    }
   }
 }
 </script>
