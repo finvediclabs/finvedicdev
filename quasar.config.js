070e55aa5279ae249f114020ce18b2c79904ccc8
finvedicdev <128971579+finvedicdev@users.noreply.github.com>
2023-12-11 23:40:07 +0530

    wip

--- quasar.config.js
+++ quasar.config.js
@@ -62,7 +62,7 @@ module.exports = configure(function (/* ctx */) {
 
       // rebuildCache: true, // rebuilds Vite/linter/etc cache on startup
 
-      // publicPath: '/',
+       publicPath: 'dev',
       // analyze: true,
       // env: {},
       // rawDefine: {}
