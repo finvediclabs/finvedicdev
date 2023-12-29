1b5994017126ef64e70188305fc5b1c350915d2c
finvedicdev <128971579+finvedicdev@users.noreply.github.com>
2023-12-29 21:16:16 +0530

    wip

--- src/pages/dashboard/Urls.js
+++ src/pages/dashboard/Urls.js
@@ -2,7 +2,7 @@ export const urls = {
   usersUrl: 'api/users',
   booksDataUrl: 'api/books',
   getRolesUrl: 'api/roles',
-  createVmsUrl: 'JSON/createvm',
+  createVmsUrl: 'createazurevms',
   videoDataUrl: 'api/videos',
   bookChaptersUrl: 'api/bookChapters',
   videoChaptersUrl: 'api/videoChapters',
