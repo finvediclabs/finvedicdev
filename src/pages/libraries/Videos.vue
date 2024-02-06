f9deccece24e3956f28b65523797cac84288cb72
Prathima Boddepalli <128971579+finvedicdev@users.noreply.github.com>
2024-02-06 12:07:40 +0530

    buttons size adjusted

--- src/pages/libraries/Videos.vue
+++ src/pages/libraries/Videos.vue
@@ -16,7 +16,7 @@
       -->
     </fin-portlet-header>
     <fin-portlet-item>
-      <div class="radio-button-group mts" style=" box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);border-radius: 25px;">
+      <div class="radio-button-group mts" style=" box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);border-radius: 25px;width: 60%;margin-left: auto;margin-right: auto;">
   <div class="item">
     <input type="radio" name="button-group" class="radio-button" value="1" id="button1" checked />
     <label for="button1">Introduction to Banking</label>
