14528fe33d3665f36ac9425a08c1958fedcb9f58
Prathima Boddepalli <128971579+finvedicdev@users.noreply.github.com>
2024-02-28 17:46:59 +0530

    styling of drona,adding ved training and vms front end done
    styling of portal chat bot done
    ved and drona training done
    vms change from input to button

--- src/layouts/ChatBot.vue
+++ src/layouts/ChatBot.vue
@@ -1,225 +1,243 @@
 <template>
-    <div class="chatbot" :class="{ open: isOpen }">
-      <!-- Chat interface -->
-      
-      <div class="chat-container">
-        <!-- Welcome message -->
-        <div v-if="isOpen" class="welcome-message" style="z-index: 999999;">
-          <p >Hi! I'm Dhrona, your AI virtual assistant. Ask me about Fintech?</p>
+  <div class="chatbot" :class="{ open: isOpen }">
+    <!-- Chat interface -->
+    <img src="src/assets/monk_new.png" alt="" class="monk-image">
+    <div class="chat-container">
+      <!-- Welcome message -->
+
+      <div v-if="isOpen" class="welcome-message" style="z-index: 999999;">
+        <p>Hi! I'm Drona, your AI virtual assistant.<br> Ask me about Fintech?</p>
+      </div>
+      <!-- Messages -->
+      <div class="messages">
+        <div v-for="message in messages" :key="message.id" :class="message.type">
+          {{ message.text }}
         </div>
-  
-        <!-- Messages -->
-        <div class="messages">
-          <div v-for="message in messages" :key="message.id" :class="message.type">
-            {{ message.text }}
-          </div>
-        </div>
-  
-        <!-- Input field for user to type messages -->
-        <input type="text" v-model="newMessage" @keyup.enter="sendMessage" placeholder="Enter your Query..." class="input-field" style="float: right;">
-    
+      </div>
+      <!-- Input field for user to type messages -->
+      <input type="text" v-model="newMessage" @keyup.enter="sendMessage" placeholder="Enter your Query..." class="input-field" style="float: right;">
+     
     </div>
-  
-      <!-- Button to toggle chatbot visibility -->
-      <button class="toggle-button" @click="toggleChatbot">
-        <template v-if="isOpen">👴</template> <!-- Monk icon -->
-        <template v-else>👤</template> <!-- Person icon -->
-      </button>
-    </div>
-  </template>
-  
-  <script>
-  export default {
-    data() {
-      return {
-        isOpen: false,
-        newMessage: '',
-        messages: []
-      };
+    <!-- Button to toggle chatbot visibility -->
+    <button class="toggle-button" @click="toggleChatbot">
+      <template v-if="isOpen">
+        <img :src="isOpen ? 'src/assets/monk_small.png' : 'src/assets/monk_small_mirrored.png'" alt="Monk Icon" style="background-color: red;">
+      </template>
+      <template v-else>
+        <img src="src/assets/monk_small.png" alt="Person Icon" style="transform: scaleX(-1);">
+      </template>
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      isOpen: false,
+      newMessage: '',
+      messages: []
+    };
+  },
+  methods: {
+    toggleChatbot() {
+      this.isOpen = !this.isOpen;
     },
-    methods: {
-      toggleChatbot() {
-        this.isOpen = !this.isOpen;
-      },
-      sendMessage() {
-  const message = this.newMessage.trim();
-  if (!message) return; // Don't send empty messages
-
-  // Push the outgoing message to the messages array immediately
-  const outgoingMessage = { text: message, type: 'outgoing', id: Date.now() };
-  this.messages.push(outgoingMessage);
-  console.log('Outgoing message:', outgoingMessage); // Log outgoing message
-  this.newMessage = ''; // Clear the input field
-
-  // Send the message to the API
-  const formData = new FormData();
-  formData.append('query', message); // Append the query parameter
-  formData.append('source', 'WEBSITE'); // Append the source parameter
-  fetch('https://fnbackend.finvedic.com/api/bot/query', {
-    method: 'POST',
-    body: formData // Send form data
-  })
-  .then(response => {
-    if (!response.ok) {
-      throw new Error('Network response was not ok');
+    sendMessage() {
+      const message = this.newMessage.trim();
+      if (!message) return; // Don't send empty messages
+      // Push the outgoing message to the messages array immediately
+      const outgoingMessage = { text: message, type: 'outgoing', id: Date.now() };
+      this.messages.push(outgoingMessage);
+      console.log('Outgoing message:', outgoingMessage); // Log outgoing message
+      this.newMessage = ''; // Clear the input field
+      // Send the message to the API
+      const formData = new FormData();
+      formData.append('query', message); // Append the query parameter
+      formData.append('source', 'PORTAL'); // Append the source parameter
+      fetch('https://fnbackend.finvedic.com/api/bot/query', {
+        method: 'POST',
+        body: formData // Send form data
+      })
+        .then(response => {
+          if (!response.ok) {
+            throw new Error('Network response was not ok');
+          }
+          return response.json();
+        })
+        .then(data => {
+          // Handle the response from the API
+          const responseData = data.data; // Extract the 'data' field from the API response
+          const incomingMessage = { text: responseData, type: 'incoming', id: Date.now() };
+          this.messages.push(incomingMessage);
+          console.log('Incoming message:', incomingMessage); // Log incoming message
+        })
+        .catch(error => {
+          console.error('There was a problem with the fetch operation:', error);
+          // Optionally, you can add an error message to the messages array
+          const errorMessage = { text: 'Error sending message', type: 'incoming', id: Date.now() };
+          this.messages.push(errorMessage);
+          console.log('Error message:', errorMessage); // Log error message
+        });
     }
-    return response.json();
-  })
-  .then(data => {
-    // Handle the response from the API
-    const responseData = data.data; // Extract the 'data' field from the API response
-    const incomingMessage = { text: responseData, type: 'incoming', id: Date.now() };
-    this.messages.push(incomingMessage);
-    console.log('Incoming message:', incomingMessage); // Log incoming message
-})
-  .catch(error => {
-    console.error('There was a problem with the fetch operation:', error);
-    // Optionally, you can add an error message to the messages array
-    const errorMessage = { text: 'Error sending message', type: 'incoming', id: Date.now() };
-    this.messages.push(errorMessage);
-    console.log('Error message:', errorMessage); // Log error message
-  });
-}
+  }
+};
+</script>
 
-    }
-  };
-  </script>
-  
-  <style scoped>
+<style scoped>
   .chatbot {
     position: fixed;
-    bottom: 70px;
+    bottom: 100px;
     right: 20px;
     z-index: 9999;
   }
-  
-  .chat-container  {
+
+  .chat-container {
     display: none;
-  background-color: #fff;
-  border: 1px solid #ccc;
-  border-radius: 10px;
- 
-  max-height: 500px; /* Limit height to enable scrolling */
-  overflow-y: auto; /* Enable vertical scrolling */
-  width: 300px; /* Fixed width */
-  height: 500px; /* Fixed height */
-  position: relative; /* Establish positioning context */
+    background-color: #fff;
+    border: 4px solid #5479F7;
+    border-radius: 20px;
+    max-height: 400px; /* Limit height to enable scrolling */
+    overflow-y: auto; /* Enable vertical scrolling */
+    width: 300px; /* Fixed width */
+    height: 400px; /* Fixed height */
+    position: relative; /* Establish positioning context */
+    padding-bottom: 40px; /* Ensure space for the image */
   }
-  
+
   .chatbot.open .chat-container {
     display: block;
   }
-  
+  .chatbot.open .monk-image{
+    display: block;
+  }
+
   .welcome-message {
+    padding-top: 10px;
     margin-bottom: 10px;
     text-align: center;
-    background-color: #5479F7;
+    background-color: #5479f7;
     width: 100%;
     color: #fff;
     font-weight: 600;
     position: fixed; /* Position the input field */
-    width:300px ;
-    border-top-right-radius:10px ;
-    border-top-left-radius:10px ;
-
-    
+    width: 294px;
+    border-top-right-radius: 12px;
+    border-top-left-radius: 12px;
   }
-  
+
   .messages {
     margin-bottom: 10px;
     margin-top: 75px;
   }
-  
+
   .messages div {
-  margin: 5px 0;
-  padding: 5px 10px;
-  border-radius: 5px;
-}
+    margin: 5px 0;
+    padding: 5px 10px;
+    border-radius: 5px;
+  }
 
-.messages .incoming {
-  color: white;
-      padding: 14px 16px;
-      line-height: 18px;
-      font-size: 14px;
-      border-radius: 7px;
-      margin-bottom: 30px;
-      margin-right: auto;
-      width: 90%;
-      position: relative;
-      background: #007bff;
-      
-      &:after {
-        bottom: 100%;
-        left: 7%;
-        border: solid transparent;
-        content: " ";
-        height: 0;
-        width: 0;
-        position: absolute;
-        pointer-events: none;
-        border-bottom-color: #007bff;
-        border-width: 10px;
-        margin-left: -10px;
-      }
+  .messages .incoming {
+    color: white;
+    padding: 14px 16px;
+    line-height: 18px;
+    font-size: 14px;
+    border-radius: 7px;
+    margin-bottom: 30px;
+    margin-right: auto;
+    width: 90%;
+    position: relative;
+    margin-left: 4%;
+    background: #007bff;
+    &:after {
+      bottom: 100%;
+      left: 7%;
+      border: solid transparent;
+      content: " ";
+      height: 0;
+      width: 0;
+      position: absolute;
+      pointer-events: none;
+      border-bottom-color: #007bff;
+      border-width: 10px;
+      margin-left: -10px;
     }
+  }
 
-.messages .outgoing {
-      color: white;
-      padding: 14px 18px;
-      line-height: 18px;
-      font-size: 14px;
-      border-radius: 7px;
-      margin-bottom: 30px;
-      margin-left: auto;
-      width: 90%;
-      position: relative;
-      background: #9db1f1;
-      
-      &:after {
-        bottom: 100%;
-        right: 7%;
-        border: solid transparent;
-        content: " ";
-        height: 0;
-        width: 0;
-        position: absolute;
-        pointer-events: none;
-        border-bottom-color: #9db1f1;
-        border-width: 10px;
-        margin-left: -10px;
-      }
-}
-  
+  .messages .outgoing {
+    color: white;
+    padding: 14px 18px;
+    line-height: 18px;
+    font-size: 14px;
+    border-radius: 7px;
+    margin-bottom: 30px;
+    margin-left: auto;
+    width: 90%;
+    position: relative;
+    background: #9db1f1;
+    margin-right: 4%;
+    &:after {
+      bottom: 100%;
+      right: 7%;
+      border: solid transparent;
+      content: " ";
+      height: 0;
+      width: 0;
+      position: absolute;
+      pointer-events: none;
+      border-bottom-color: #9db1f1;
+      border-width: 10px;
+      margin-left: -10px;
+    }
+  }
+
   .toggle-button {
     position: fixed;
     bottom: 20px;
     right: 20px;
-    background-color: #007bff;
-    color: white;
+    width: 80px;
+    height: 70px;
+    background-color: transparent;
     border: none;
-    border-radius: 50%;
-    width: 50px;
-    height: 50px;
-    font-size: 20px;
-    text-align: center;
-    line-height: 50px;
     cursor: pointer;
   }
-  
+
+  .toggle-button img {
+    width: 100%;
+    height: 100%;
+    border-radius: 50%;
+    background-color: #007bff;
+  }
+
+  .toggle-button:focus {
+    outline: none;
+  }
+
   .input-field {
     position: fixed; /* Position the input field */
-    width:300px ;
-  bottom: 0; /* Align to the bottom */
-  margin: 70px 0px; /* Add margin for better spacing */
-  border: 2px solid #ccc;
-    border-radius: 20px;
+    width: 294px;
+    bottom: 32px; /* Align to the bottom */
+    margin: 72px 0px; /* Add margin for better spacing */
+    border: 2px solid #5479F7;
+    border-radius: 15px;
     outline: none;
     height: 40px;
     transition: border-color 0.3s ease;
   }
+
   .input-field:focus {
     border-color: #007bff; /* Change border color on focus */
   }
 
-  </style>
-  
+  .monk-image {
+    position: absolute;
+    display: none;
+    bottom: 32px;
+    left: 0; /* Position at the horizontal center of the container */
+    margin-left: -80px; /* Adjust image width */
+    width: 110px;
+    transform: scaleX(-1);
+    z-index: 3;
+     /* Ensure the image is above other content */
+  }
+</style>
---
+++ src/pages/dashboard/ai_upload/VedTraining.vue
@@ -0,0 +1,175 @@
+<template>
+    <div
+      class="drop-container"
+      @dragover.prevent
+      @dragenter.prevent
+      @drop="handleDrop"
+    >
+      <input
+        type="file"
+        @change="handleFileUpload"
+        accept="application/pdf"
+        style="display: none;"
+        ref="fileInput"
+      />
+      <div class="btn border-gradient-blue-purple" style="position: absolute; top: 45px;left: 75%;
+    transform: translateX(-50%);width: 20%;">
+        <span>Ved Training</span>
+    </div>
+      <div style="display: flex; flex-direction: column; align-items: center; padding-top: 60px;">
+  <p style="font-size: 32px; margin-bottom: 20px;">Drag and drop PDF file here, or</p>
+  <button style="font-size: 32px; background: none; border:2px solid grey;border-radius:20px;cursor:pointer" @click="openFilePicker">
+    <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-file-type-pdf" width="120" height="120" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+  <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+  <path d="M14 3v4a1 1 0 0 0 1 1h4" />
+  <path d="M5 12v-7a2 2 0 0 1 2 -2h7l5 5v4" />
+  <path d="M5 18h1.5a1.5 1.5 0 0 0 0 -3h-1.5v6" />
+  <path d="M17 18h2" />
+  <path d="M20 15h-3v6" />
+  <path d="M11 15v6h1a2 2 0 0 0 2 -2v-2a2 2 0 0 0 -2 -2h-1z" />
+</svg>
+
+</button>
+
+</div>
+
+
+<button style="background-color: #5479F7; border-radius: 20px;font-size:32px;font-weight:600;color:#ffff;padding:0px 30px;margin-top:40px;border:none;cursor:pointer" @click="uploadPDF">Upload</button>
+
+      <div v-if="pdfUrl">
+        <h2>Uploaded PDF:</h2>
+        <embed :src="pdfUrl" type="application/pdf" width="600" height="400" />
+      </div>
+      <div v-if="notification" class="notification">
+        {{ notification }}
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  import axios from 'axios';
+  
+  export default {
+    data() {
+      return {
+        selectedFile: null,
+        pdfUrl: null,
+        notification: null,
+      };
+    },
+    methods: {
+      handleFileUpload(event) {
+        this.selectedFile = event.target.files[0];
+      },
+      handleDrop(event) {
+        event.preventDefault();
+        this.selectedFile = event.dataTransfer.files[0];
+      },
+      openFilePicker() {
+        this.$refs.fileInput.click();
+      },
+      async uploadPDF() {
+        if (!this.selectedFile) {
+          this.notification = "Please select a PDF file to upload."; // Set notification message
+          setTimeout(() => {
+            this.notification = null; // Clear notification after a delay
+          }, 3000); // Adjust the delay as needed
+          return;
+        }
+  
+        const formData = new FormData();
+        formData.append("file", this.selectedFile);
+        formData.append("source", "website");
+  
+        try {
+          const response = await axios.post("https://fnbackend.finvedic.com/api/bot", formData, {
+            headers: {
+              'Content-Type': 'multipart/form-data'
+            }
+          });
+  
+          // Assuming the response contains a URL for the uploaded file
+          this.pdfUrl = response.data.url;
+          this.notification = "PDF uploaded successfully"; // Set notification message
+          setTimeout(() => {
+            this.notification = null; // Clear notification after a delay
+          }, 3000); // Adjust the delay as needed
+        } catch (error) {
+          console.error("Error uploading PDF:", error);
+          // Handle error
+        }
+      },
+    },
+  };
+  </script>
+  
+  <style scoped>
+  
+  .border-gradient-blue-purple {
+      background-image: linear-gradient(120deg, #33ccff 0%, #841eeb 100%);
+  }
+  .btn {
+      font-size: 1rem;
+      font-weight: 600;
+      border-radius: 999px;
+    box-sizing: border-box;
+      color: #1a1a1a;
+      display: inline-block;
+      padding: 4px;
+      position: relative;
+    text-decoration: none;
+      text-transform: uppercase;
+  }
+  
+  .btn > span {
+      align-items: center;
+      background: #f8f8f8;
+      border-radius: 999px;
+    display: flex;
+    justify-content: center;
+    transition: background .5s ease;
+    padding: 1rem 2rem;
+  }
+  
+  .btn:hover > span {
+      background: transparent;
+  }
+  .btn:hover {
+      color: #fff;
+  }
+  .drop-container {
+      margin-top: 80px;
+      margin-right: 40px;
+      margin-left: 40px;
+      min-height: 400px;
+      text-align: center;
+      border: 3px solid #5479F7; /* Set initial border */
+      border-radius: 24px;
+      background: #ececec;/* Define gradient colors */
+     
+      background-clip: padding-box, border-box; /* Clip gradient to border */
+      animation: rotate-color 5s linear infinite; /* Apply rotation animation */
+      
+  }
+  
+  
+    
+    .drop-container p {
+      margin: 0;
+    }
+    
+    .drop-container button {
+      margin-top: 10px;
+    }
+    
+    .notification {
+      background-color: #4caf50;
+      color: white;
+      padding: 16px;
+      border-radius: 5px;
+      position: fixed;
+      bottom: 60px;
+      right: 20px;
+    }
+    </style>
+  
