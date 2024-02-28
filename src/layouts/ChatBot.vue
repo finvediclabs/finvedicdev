85d205f5061c17963d04cd29731f0b75ae820189
Prathima Boddepalli <128971579+finvedicdev@users.noreply.github.com>
2024-02-28 18:00:58 +0530

    images_reupload

--- src/layouts/ChatBot.vue
+++ src/layouts/ChatBot.vue
@@ -21,10 +21,10 @@
     <!-- Button to toggle chatbot visibility -->
     <button class="toggle-button" @click="toggleChatbot">
       <template v-if="isOpen">
-        <img :src="isOpen ? 'src/assets/monk_small_new.png' : 'src/assets/monk_small_mirrored_new.png'" alt="Monk Icon" style="background-color: red;">
+        <img :src="isOpen ? 'src/assets/monk_small.png' : 'src/assets/monk_small_mirrored.png'" alt="Monk Icon" style="background-color: red;">
       </template>
       <template v-else>
-        <img src="src/assets/monk_small_new.png" alt="Person Icon" style="transform: scaleX(-1);">
+        <img src="src/assets/monk_small.png" alt="Person Icon" style="transform: scaleX(-1);">
       </template>
     </button>
   </div>
