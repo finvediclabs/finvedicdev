299214f77d3796f7bb1d4df863e94a978e12aaf7
Sandeep Kuppili <sandeep.kuppili@finvedic.com>
2024-09-20 18:58:04 +0530

    Update MainDashboard.vue

--- src/pages/dashboard/MainDashboard.vue
+++ src/pages/dashboard/MainDashboard.vue
@@ -127,6 +127,13 @@ export default {
           enable: true
         },
         {
+          image: videosPng,
+          title: 'Notifications',
+          description: 'Perfect for learning how the framework works.',
+          path: 'notifications',
+          background: '#D3D3D390',
+        },
+        {
           image: organizationPng,
           title: 'Website Forms Data',
           description: 'Perfect for learning how the framework works.',
